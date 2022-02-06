c523e53f7285ec029722142a661420bd4316caff
Seth Rasmusson <rasmusso.seth@uwlax.edu>
2022-02-06 16:42:09 -0600

    references to jdb

--- Database/src/Main.java
+++ Database/src/Main.java
@@ -1,7 +1,7 @@
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String args){
+    public static void main(String[] args){
         DatabaseConnection con = new DatabaseConnection();
         try {
             con.initialize();
