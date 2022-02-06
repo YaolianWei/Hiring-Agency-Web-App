4954cb3fc6689e251ef05efae754e3b017abd37f
Seth Rasmusson <rasmusso.seth@uwlax.edu>
2022-02-05 18:42:07 -0600

    references to jdb

--- Database/src/DatabaseConnection.java
+++ Database/src/DatabaseConnection.java
@@ -1,9 +1,22 @@
+import java.sql.*;
+
+
 public class DatabaseConnection {
+
+    private Connection connection = null;
+    private String url = "jdbc:sqlite:Agency.db";
+
     public DatabaseConnection(){
 
     }
 
-    public void initialize() {
-
+    public void initialize() throws SQLException {
+        connection = DriverManager.getConnection(url);
+        System.out.println("connection is " + connection.isClosed());
     }
+
+    public void disconnect() throws SQLException {
+        connection.close();
+    }
+
 }
---
+++ Database/src/Main.java
@@ -0,0 +1,12 @@
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String args){
+        DatabaseConnection con = new DatabaseConnection();
+        try {
+            con.initialize();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
