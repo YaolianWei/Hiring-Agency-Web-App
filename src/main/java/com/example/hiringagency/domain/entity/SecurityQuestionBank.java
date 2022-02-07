c88bf49f90a6e2129e3c34ae262c103c2b3210f1
Yaolian Wei <wei5927@uwlax.edu>
2022-02-07 05:55:04 -0600

    #1

---
+++ src/main/java/com/example/hiringagency/domain/entity/SecurityQuestionBank.java
@@ -0,0 +1,44 @@
+package com.example.hiringagency.domain.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description  
+ * @Author Yaolian
+ * @Date 2022-02-07 
+ */
+
+@Data
+@NoArgsConstructor
+@Table ( name ="SecurityQuestionBank" )
+public class SecurityQuestionBank  implements Serializable {
+
+	private static final long serialVersionUID =  4734588478222814739L;
+
+   	@Column(name = "BankQuestionID" )
+	private Long bankQuestionId;
+
+   	@Column(name = "Question" )
+	private String question;
+
+	public Long getBankQuestionId() {
+		return this.bankQuestionId;
+	}
+
+	public void setBankQuestionId(Long bankQuestionId) {
+		this.bankQuestionId = bankQuestionId;
+	}
+
+	public String getQuestion() {
+		return this.question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+}
---
+++ src/main/java/com/example/hiringagency/domain/entity/SecurityQuestions.java
@@ -0,0 +1,66 @@
+package com.example.hiringagency.domain.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description  
+ * @Author Yaolian
+ * @Date 2022-02-07 
+ */
+
+@Data
+@NoArgsConstructor
+@Table ( name ="SecurityQuestions" )
+public class SecurityQuestions  implements Serializable {
+
+	private static final long serialVersionUID =  5685392052734455560L;
+
+   	@Column(name = "QuestionID" )
+	private Long QuestionID;
+
+   	@Column(name = "Question" )
+	private String Question;
+
+   	@Column(name = "Answer" )
+	private String Answer;
+
+   	@Column(name = "UserID" )
+	private String UserID;
+
+	public Long getQuestionId() {
+		return this.QuestionID;
+	}
+
+	public void setQuestionId(Long questionId) {
+		this.QuestionID = questionId;
+	}
+
+	public String getQuestion() {
+		return this.Question;
+	}
+
+	public void setQuestion(String question) {
+		this.Question = question;
+	}
+
+	public String getAnswer() {
+		return this.Answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.Answer = answer;
+	}
+
+	public String getUserID() {
+		return this.UserID;
+	}
+
+	public void setUserID(String userID) {
+		this.UserID = userID;
+	}
+
+}
